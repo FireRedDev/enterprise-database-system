cd2713a458e94aea53864cd4b2df57a118344b34
hmoerth0900 <74739291+hmoerth0900@users.noreply.github.com>
2021-11-24 17:56:39 +0100

    create/delete credentials when saving new users

--- src/main/java/at/jku/employeeonboardingsystem/service/impl/SystemuserServiceImpl.java
+++ src/main/java/at/jku/employeeonboardingsystem/service/impl/SystemuserServiceImpl.java
@@ -1,12 +1,17 @@
 package at.jku.employeeonboardingsystem.service.impl;
 
+import at.jku.employeeonboardingsystem.domain.Department;
 import at.jku.employeeonboardingsystem.domain.Systemuser;
 import at.jku.employeeonboardingsystem.domain.Targetsystemcredentials;
+import at.jku.employeeonboardingsystem.repository.DepartmentRepository;
 import at.jku.employeeonboardingsystem.repository.SystemuserRepository;
 import at.jku.employeeonboardingsystem.repository.TargetsystemcredentialsRepository;
 import at.jku.employeeonboardingsystem.service.SystemuserService;
 import java.io.Console;
+import java.util.*;
 import java.util.Optional;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.bouncycastle.jcajce.provider.asymmetric.dsa.DSASigner.detDSA;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -27,23 +32,43 @@ public class SystemuserServiceImpl implements SystemuserService {
 
     private final TargetsystemcredentialsRepository targetsystemcredentialsRepository;
 
+    private final DepartmentRepository departmentRepository;
+
     public SystemuserServiceImpl(
         SystemuserRepository systemuserRepository,
-        TargetsystemcredentialsRepository targetsystemcredentialsRepository
+        TargetsystemcredentialsRepository targetsystemcredentialsRepository,
+        DepartmentRepository departmentRepository
     ) {
         this.systemuserRepository = systemuserRepository;
         this.targetsystemcredentialsRepository = targetsystemcredentialsRepository;
+        this.departmentRepository = departmentRepository;
     }
 
     @Override
     public Systemuser save(Systemuser systemuser) {
         log.debug("Request to save Systemuser : {}", systemuser);
-        Targetsystemcredentials targetsystemcredentials = new Targetsystemcredentials();
-        targetsystemcredentials.setSystemuser(systemuser);
-        targetsystemcredentials.setUsername("test");
-        targetsystemcredentials.setPassword("test1");
-        log.debug("request to save credentials: {}", targetsystemcredentials);
-        //  targetsystemcredentialsRepository.save(targetsystemcredentials);
+
+        List<Targetsystemcredentials> credentials = new ArrayList<>();
+        systemuser
+            .getDepartments()
+            .forEach(d -> {
+                departmentRepository
+                    .findOneWithEagerRelationships(d.getId())
+                    .get()
+                    .getTargetsystems()
+                    .forEach(t -> {
+                        Targetsystemcredentials targetsystemcredentials = new Targetsystemcredentials();
+                        targetsystemcredentials.setSystemuser(systemuser);
+                        targetsystemcredentials.setUsername(systemuser.getName().replace(' ', '_').toLowerCase());
+                        String generatedString = RandomStringUtils.random(10, true, true);
+                        targetsystemcredentials.setPassword(generatedString);
+                        targetsystemcredentials.setTargetsystem(t);
+                        credentials.add(targetsystemcredentials);
+                    });
+            });
+
+        targetsystemcredentialsRepository.saveAll(credentials);
+
         return systemuserRepository.save(systemuser);
     }
 
@@ -93,6 +118,9 @@ public class SystemuserServiceImpl implements SystemuserService {
     @Override
     public void delete(Long id) {
         log.debug("Request to delete Systemuser : {}", id);
+        Systemuser systemuser = systemuserRepository.findById(id).orElseThrow();
+        List<Targetsystemcredentials> cre = targetsystemcredentialsRepository.findBySystemuser(systemuser);
+        cre.forEach(c -> targetsystemcredentialsRepository.delete(c));
         systemuserRepository.deleteById(id);
     }
 }
