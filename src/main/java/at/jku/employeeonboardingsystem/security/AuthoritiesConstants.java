1a19571b11773471e3b7d47dcb708a25797972f7
hmoerth0900 <74739291+hmoerth0900@users.noreply.github.com>
2021-12-01 15:52:19 +0100

    added roles

--- src/main/java/at/jku/employeeonboardingsystem/security/AuthoritiesConstants.java
+++ src/main/java/at/jku/employeeonboardingsystem/security/AuthoritiesConstants.java
@@ -9,6 +9,10 @@ public final class AuthoritiesConstants {
 
     public static final String USER = "ROLE_USER";
 
+    public static final String HR = "ROLE_HR";
+
+    public static final String IT = "ROLE_IT";
+
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
     private AuthoritiesConstants() {}
